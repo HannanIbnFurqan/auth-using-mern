71826dfd0910fbabf677a36997ac48aad02ad5a8
HannanIbnFurqan <mohdhannan7913@gmail.com>
2024-10-04 00:58:01 +0530

    Normalize line endings

---
+++ routes/userRoute.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import userController from '../controller/user.controller.js';
+
+const router = express.Router();
+
+// Register route
+router.post('/register', userController.register);
+
+// Route to refresh the access token using the refresh token
+router.get('/refresh_token', userController.refreshToken);
+
+export default router;
