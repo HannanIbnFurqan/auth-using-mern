2caa790bf18a8b608693f55096c85c9bb0801cf0
HannanIbnFurqan <mohdhannan7913@gmail.com>
2024-10-04 01:37:20 +0530

    Complete to develop the login functionality

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -74,8 +74,19 @@ const userController = {
     // create login functionality
 
     login: async (req,res)=>{
+       try {
         const {email,password} = req.body
         const user = await User.findOne({email})
+        if(!user) return res.status(400).json({message: "user does not exist"})
+        const isMatch = await bcrypt.compare(password, user.password)
+       if(!isMatch) return res.status(400).json({message:"incorrect password"})
+
+        res.status(200).json({message: 'login success'})
+        
+       } catch (error) {
+         return res.status(500).json({message: error.message})
+       }
+
     }
 };
 
