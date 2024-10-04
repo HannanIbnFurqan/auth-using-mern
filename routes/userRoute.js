ad764c7d55e57997edec2ab1914b6639023f290f
HannanIbnFurqan <mohdhannan7913@gmail.com>
2024-10-04 11:42:19 +0530

    add login route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,6 +5,8 @@ const router = express.Router();
 
 // Register route
 router.post('/register', userController.register);
+// login route
+router.post('/register', userController.login);
 
 // Route to refresh the access token using the refresh token
 router.get('/refresh_token', userController.refreshToken);
