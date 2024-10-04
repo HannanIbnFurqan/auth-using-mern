307159a1e653083b2a957c78b977c8ed46ee01c0
HannanIbnFurqan <mohdhannan7913@gmail.com>
2024-10-04 12:48:30 +0530

    update login route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,7 +6,7 @@ const router = express.Router();
 // Register route
 router.post('/register', userController.register);
 // login route
-router.post('/register', userController.login);
+router.post('/login', userController.login);
 
 // Route to refresh the access token using the refresh token
 router.get('/refresh_token', userController.refreshToken);
